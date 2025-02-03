037c0e11848eafd3e38e133741ddc59ca5e7651e
Priyansu Sahoo <priyansusahoo1@gmail.com>
2025-02-03 22:05:45 +0530

    Thread Name

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@ class Main {
         System.out.println("This is the Main thread and name is : " + Thread.currentThread().getName());
         MyRunnable r = new MyRunnable();
         Thread t = new Thread(r, "My-Cutie-Pie-Process");
-        
+        t.start();
+        // At this point of time, ther are 2 threads
         System.out.println("This is the Child thread and name is : " + t.getName());
     }
 }
@@ -13,5 +14,6 @@ class MyRunnable implements Runnable {
     @Override
     public void run(){
         System.out.println("Child Thread");
+        System.out.println("Child Thread - " + Thread.currentThread().getName());
     }
 }
